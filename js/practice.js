89416dc6a8626dfdd7b06c5f82d7e31c7bc620bf
odeydev <odeyijuo84@gmail.com>
2026-08-07 19:36:25 +0100

    new add ons

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,6 @@
-const h1 = document.querySelector('h1');
+// challenge no 1
+const heading = document.querySelector('h1');
+
+const balance = document.querySelector('.balance');
+
+const changeBtn = document.querySelector('#change-btn');
